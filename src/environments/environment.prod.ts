53ec31ad42f85b02bd8d1cf2b147f1758abe94b7
Bossa Web Solutions <gvfbossa@hotmail.com>
2025-03-14 10:55:04 -0300

    ajuste deploy railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://credito-centralconsig-be-production-53cb.up.railway.appapi'
+    apiUrl: 'https://credito-centralconsig-be-production-53cb.up.railway.app/api'
   };
   
